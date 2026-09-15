5ba9a3b8535fbadb44c400c6978981cc8a011f2e
Drew Mochak <amockery@gmail.com>
2026-09-14 23:50:28 -0400

    Update patch metadata for Morphe

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -3,10 +3,11 @@ group = "com.objectinspace.audible"
 patches {
     about {
         name = "Audible Home Theater Atmos"
-        description = "Private ReVanced patch preferring E-AC-3/JOC spatial audio in Audible 26.36.09"
-        source = "https://github.com/ObjectInSpace/audible-HT-Atmos"
+        description = "Private Morphe patch preferring E-AC-3/JOC spatial audio in Audible 26.36.09"
+        source = "git@github.com:ObjectInSpace/audible-HT-Atmos.git"
         author = "ObjectInSpace"
+        contact = "na"
         website = "https://github.com/ObjectInSpace/audible-HT-Atmos"
-        license = "All rights reserved (private prototype)"
+        license = "GPLv3"
     }
 }
