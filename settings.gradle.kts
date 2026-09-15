f4da3ec4fa90cc7bcc4b63704e73236b7ae491bb
Drew Mochak <amockery@gmail.com>
2026-09-14 23:50:23 -0400

    Migrate build to Morphe patch plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "audible-ht-atmos"
+rootProject.name = "audible-HT-Atmos"
 
 pluginManagement {
     repositories {
@@ -6,15 +6,16 @@ pluginManagement {
         google()
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/revanced/registry")
+            url = uri("https://maven.pkg.github.com/MorpheApp/registry")
             credentials {
                 username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")
                 password = providers.gradleProperty("gpr.key").orNull ?: System.getenv("GITHUB_TOKEN")
             }
         }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
 plugins {
-    id("app.revanced.patches") version "1.0.0-dev.5"
+    id("app.morphe.patches") version "1.3.4"
 }
