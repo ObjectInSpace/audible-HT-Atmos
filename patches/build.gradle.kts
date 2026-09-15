60b5cc39ac4162d66c13c4a4a3f637b88cd0d8cd
Drew Mochak <amockery@gmail.com>
2026-09-14 23:46:14 -0400

    Add patch bundle metadata

---
+++ patches/build.gradle.kts
@@ -0,0 +1,12 @@
+group = "com.objectinspace.audible"
+
+patches {
+    about {
+        name = "Audible Home Theater Atmos"
+        description = "Private ReVanced patch preferring E-AC-3/JOC spatial audio in Audible 26.36.09"
+        source = "https://github.com/ObjectInSpace/audible-HT-Atmos"
+        author = "ObjectInSpace"
+        website = "https://github.com/ObjectInSpace/audible-HT-Atmos"
+        license = "All rights reserved (private prototype)"
+    }
+}
